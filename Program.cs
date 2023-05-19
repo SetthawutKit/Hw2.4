127fb806b02955128edd676914fdb330bf352946
SetthawutKit <122444852+SetthawutKit@users.noreply.github.com>
2023-05-20 00:10:29 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class MainClass {
+    public static void Main(string[] args) {
+        string[] inputs = Console.ReadLine().Split(' ');
+        int N = int.Parse(inputs[0]);
+        int K = int.Parse(inputs[1]);
+
+        inputs = Console.ReadLine().Split(' ');
+        int[] population = Array.ConvertAll(inputs, int.Parse);
+
+        int maxCustomers = 0;
+        for (int i = 0; i < N; i++) {
+            int totalCustomers = CalculateTotalCustomers(population, N, K, i);
+            if (totalCustomers > maxCustomers) {
+                maxCustomers = totalCustomers;
+            }
+        }
+
+        Console.WriteLine(maxCustomers);
+    }
+
+    static int CalculateTotalCustomers(int[] population, int N, int K, int index) {
+        int totalCustomers = 0;
+        for (int j = index - K; j <= index + K; j++) {
+            if (j >= 0 && j < N) {
+                totalCustomers += population[j];
+            }
+        }
+        return totalCustomers;
+    }
+}
